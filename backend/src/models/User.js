d05c2db910e9cdcf6e3e59dd5c7f7be187c6a664
terobassem <terobassem.5@gmail.com>
2026-05-27 01:17:06 +0300

    fixed login bug

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -4,7 +4,7 @@ import bcrypt from 'bcryptjs';
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
-    age: { type: Number, required: true, min: 4, max: 18 },
+    age: { type: Number, required: true, min: 4, max: 50 },
     grade: { type: String, required: true, trim: true },
     phone: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
